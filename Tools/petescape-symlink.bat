2c1fd433717e95cb4b014d2742f04969863fa217
Zee <cucflavius@gmail.com>
2025-02-02 13:20:10 +0200

    Move to nested folder, and add symlink

---
+++ Tools/petescape-symlink.bat
@@ -0,0 +1,3 @@
+mklink /d "E:\Games\World of Warcraft\_retail_\Interface\AddOns\PetEscape" "D:\PetEscape\PetEscape"
+mklink /d "E:\Games\World of Warcraft\_ptr_\Interface\AddOns\PetEscape" "D:\PetEscape\PetEscape"
+mklink /d "E:\Games\World of Warcraft\_xptr_\Interface\AddOns\PetEscape" "D:\PetEscape\PetEscape"
